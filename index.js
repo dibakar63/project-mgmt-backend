3f860da3f1687b939730dfde85c14d7815044b2b
dibakar63 <dibakardey63@gmail.com>
2023-03-31 15:58:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const colors=require('colors');
+const cors = require('cors');
+const connectDB=require('./config/db')
+require('dotenv').config();
+const {graphqlHTTP} =require('express-graphql');
+const schema=require('./schema/schema');
+const port=process.env.PORT || 5000; 
+
+const app=express();
+
+//connect to database
+
+connectDB();
+app.use(cors());
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV='developement'
+}))
+app.listen(port,console.log(`Server running on port ${port}`))
